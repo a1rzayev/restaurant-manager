16bd804341717d762876b06739535ff0dcc392d7
a1rzayev <asifrzayev2006@example.com>
2025-01-09 10:14:23 +0400

    Make simple views(categories, dishes, restaurants)

--- restaurant_manager/menu/views.py
+++ restaurant_manager/menu/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Restaurant, Category, Dish
 
-# Create your views here.
+def restaurant_list(request):
+    restaurants = Restaurant.objects.all()
+    return render(request, 'menu/restaurant_list.html', {'restaurants': restaurants})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'menu/category_list.html', {'categories': categories})
+
+def dish_list(request):
+    dishes = Dish.objects.all()
+    return render(request, 'menu/dish_list.html', {'dishes': dishes})
