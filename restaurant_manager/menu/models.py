b5663a5d4afc380f392cfd6df59a3d2273beac97
a1rzayev <asifrzayev2006@example.com>
2025-01-09 05:41:12 +0400

    Add models and register them

--- restaurant_manager/menu/models.py
+++ restaurant_manager/menu/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
