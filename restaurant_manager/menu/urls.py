c39338d020b955fd57241490c3a6f4471b07e392
a1rzayev <asifrzayev2006@example.com>
2025-01-09 11:19:04 +0400

    Configure addition forms

--- restaurant_manager/menu/urls.py
+++ restaurant_manager/menu/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # Admin paths
     path('admin/register/', views.admin_register, name='admin_register'),
     path('admin/login/', views.admin_login, name='admin_login'),
-    path('admin/add_restaurant/', views.add_restaurant, name='add_restaurant'),
-    path('admin/add_category/', views.add_category, name='add_category'),
-    path('admin/add_dish/', views.add_dish, name='add_dish'),
+    path('admin/menu/restaurant/add/', views.add_restaurant, name='add_restaurant'),
+    path('admin/menu/category/add/', views.add_category, name='add_category'),
+    path('admin/menu/dish/add/', views.add_dish, name='add_dish'),
     path('admin/delete_restaurant/<int:id>/', views.delete_restaurant, name='delete_restaurant'),
     path('admin/delete_category/<int:id>/', views.delete_category, name='delete_category'),
     path('admin/delete_dish/<int:id>/', views.delete_dish, name='delete_dish'),
